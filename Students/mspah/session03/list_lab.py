203acb840a4b5ab4f2cacb354c2279926fc44d21
mspah <mspah@zulily.com>
2015-01-26 19:08:16 -0800

    Added list lab

---
+++ Students/mspah/session03/list_lab.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+
+def list_lab_part1(fruits):
+	print fruits
+
+
+
+
+if __name__ == "__main__":
+	fruits = ["Apples", "Pears", "Oranges", "Peaches"]
+	list_lab_part1(fruits)
