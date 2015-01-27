a4756c5df8a344a9a9ec425c18cf80ee17c7824b
mspah <mspah@zulily.com>
2015-01-26 19:53:18 -0800

    Updated list_lab

--- Students/mspah/session03/list_lab.py
+++ Students/mspah/session03/list_lab.py
@@ -4,10 +4,47 @@
 
 def list_lab_part1(fruits):
 	print fruits
+	new_fruit = raw_input("Which fruit would you like to add? ")
+	fruits.append(new_fruit)
+	print fruits
+	number = int(raw_input("What fruit would like to see? (provide an interger) "))
+	print "The number %i fruit is %s" % (number, fruits[(number - 1)])
+	fruits += [ "Grapes" ]
+	print fruits
+	fruits.insert(0, "Tomato")
+	print fruits
 
+	for fruit in fruits:
+		if fruit[0] == "P":
+			print fruit
+
+	return fruits
+
+
+def remove_from_list(fruits, item):
+
+	try:
+		fruits.remove(item)
+	except:
+		print "Fruit not found"
+		item = raw_input("What fruit do you want to remove? ")
+		remove_from_list(fruits, item)
+
+
+def list_lab_part2(fruits):
+	print fruits
+	fruits.pop()
+	print fruits
+	item = raw_input("What fruit do you want to remove?")
+	remove_from_list(fruits, item)
+	print fruits
+
+def list_lab_part3(fruits):
 
 
 
 if __name__ == "__main__":
 	fruits = ["Apples", "Pears", "Oranges", "Peaches"]
-	list_lab_part1(fruits)
+	fruits = list_lab_part1(fruits)
+	list_lab_part2(fruits)
+	list_lab_part3(fruits)
