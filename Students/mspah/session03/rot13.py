323b5b8176272c68c22a6a8d98e9bf49905c5195
mspah <mspah@zulily.com>
2015-01-27 21:20:37 -0800

    added codecs library

--- Students/mspah/session03/rot13.py
+++ Students/mspah/session03/rot13.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
 
+import codecs
 
+def rot13(text, decode=True):
+	"""
+	By default rot13 will return a decoded rot13 message. If you wish to encode a message pass
+	a second argument to the rot13, for example rot13("This is text I want encoded", decode=False)
+	"""
 
-def rot13(text):
-
-
-
-
-
-
-
-
+	if decode:
+		try:
+			return codecs.decode(text, 'rot13')
+		else:
+			print "Error decoding text"
+	else:
+		try:
+			return codecs.encode(text, 'rot13')
+		else: 
+			print "Error encoding text"
 
 
 
 if __name__ == '__main__':
-	
+	encrypted_text = "Zntargvp sebz bhgfvqr arne pbeare"
+	decoded_text = rot13(encrypted_text)
+	print decoded_text
+	assert (rot13(decoded_text, decode=False) == encrpyted_text)
+
