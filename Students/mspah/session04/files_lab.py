127cda937911d1b1cd0995f60f688b8f3ba7ae3f
mspah <mspah@zulily.com>
2015-01-30 00:39:30 -0800

    files_lab.py

---
+++ Students/mspah/session04/files_lab.py
@@ -0,0 +1,17 @@
+!#/usr/bin/env python
+
+
+"""
+Files Lab: If there is time.
+
+Files Lab
+In the class repo, in:
+
+Examples\Session01\students.txt
+
+You will find the list I generated of all the students in the class, and what programming languages they have used in the past.
+
+Write a little script that reads that file, and generates a list of all the languages that have been used.
+
+Extra credit: keep track of how many students specified each language.
+""
