94a81b6f88e2a7802ee2953adcccd0eabe37d78c
mspah <mspah@zulily.com>
2015-01-31 19:15:15 -0800

    finished dict_lab

--- Students/mspah/session04/dict_lab.py
+++ Students/mspah/session04/dict_lab.py
@@ -1,30 +1,81 @@
 #!/usr/bin/env python
-"""
-1.
-create a dictionary containing “name”, “city”, and “cake” for “Chris” from “Seattle” who likes “Chocolate”.
-Display the dictionary.
-Delete the entry for “cake”.
-Display the dictionary.
-Add an entry for “fruit” with “Mango” and display the dictionary.
-Display the dictionary keys.
-Display the dictionary values.
-Display whether or not “cake” is a key in the dictionary (i.e. False) (now).
-Display whether or not “Mango” is a value in the dictionary (i.e. True).
 
-2.
-Using the dict constructor and zip, build a dictionary of numbers from zero to fifteen and the hexadecimal equivalent (string is fine).
 
-3.
-Using the dictionary from item 1: Make a dictionary using the same keys but with the number of ‘t’s in each value.
+def main():
+	part_1()
+	part_2()
+	part_3()
+	part_4()
+	part_5()
 
-4.
-Create sets s2, s3 and s4 that contain numbers from zero through twenty, divisible 2, 3 and 4.
-Display the sets.
-Display if s3 is a subset of s2 (False)
-and if s4 is a subset of s2 (True).
 
-5.
-Create a set with the letters in ‘Python’ and add ‘i’ to the set.
-Create a frozenset with the letters in ‘marathon’
-display the union and intersection of the two sets.
-"""
+
+def part_1():
+	'''
+	Dictionary lab main function
+	'''
+
+	labdict = { "name": "Chris", "city": "Seattle", "cake": "Chocolate"}
+
+	print labdict
+
+	labdict.pop("cake")
+
+	print labdict
+
+	labdict["fruit"] = "Mango"
+
+	print labdict.keys()
+	print labdict.values()
+
+	if "cake" in labdict:
+		print "Cake is present"
+
+	for key, value in labdict.items():
+		if value == "Mango":
+			print "Mango is present"
+
+def part_2():
+	'''
+	Create two arrays and create a dictonary using dict and zip
+	'''
+	n = [ x for x in range(15+1)]
+	h = [ hex(x) for x in range(15+1)]
+
+	print dict(zip(n, h))
+
+def part_3():
+	labdict = { "name": "Chris", "city": "Seattle", "cake": "Chocolate"}
+
+	for key, value in labdict.items():
+		t = value.count('t')
+		labdict[key] = t
+	else:
+		print labdict
+
+def part_4():
+
+	s2 = set([ x for x in range(21) if x % 2 == 0])
+	s3 = set([ x for x in range(21) if x % 3 == 0])
+	s4 = set([ x for x in range(21) if x % 4 == 0])
+	print s2
+	print s3
+	print s4
+
+	print s3.issubset(s2)
+	print s4.issubset(s2)
+
+def part_5():
+	set_python = set([ x for x in "Python"])
+	set_python.update("i")
+	
+	frozen_set = frozenset([x for x in "marathon"])
+
+	print set_python.union(frozen_set)
+	print set_python.intersection(frozen_set)
+
+	
+
+
+if __name__ == "__main__":
+	main()
