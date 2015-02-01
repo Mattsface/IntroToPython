c6bf5ca14fda10ce65c5bdcffa62a7aff9bdf370
mspah <mspah@zulily.com>
2015-01-31 21:01:23 -0800

    Finished files lab

--- Students/mspah/session04/files_lab.py
+++ Students/mspah/session04/files_lab.py
@@ -1,17 +1,29 @@
-!#/usr/bin/env python
+#!/usr/bin/env python
+def main():
+	try:
+		f = open('students.txt')
+	except IOerror:
+		print "Couldn't open students.txt"
 
 
-"""
-Files Lab: If there is time.
+	languages = collect_languages(f)
+	
+	for language in languages:
+		print language
 
-Files Lab
-In the class repo, in:
 
-Examples\Session01\students.txt
+def collect_languages(f):
+	l = []
 
-You will find the list I generated of all the students in the class, and what programming languages they have used in the past.
+	for line in f.readlines():
+		words = line.rstrip().split(":")
+		for languages in words[1].split(','):
+			l.append(languages.strip())
+		
+	return set(l)
 
-Write a little script that reads that file, and generates a list of all the languages that have been used.
 
-Extra credit: keep track of how many students specified each language.
-""
+
+
+if __name__ == "__main__":
+	main()
