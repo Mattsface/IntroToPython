5c91427134761511a32ee4f7623040249c707bdd
MattsFace <spahmatthew@gmail.com>
2015-02-02 21:39:14 -0800

    Added main function to kata

--- Students/mspah/session04/kata_fourteen.py
+++ Students/mspah/session04/kata_fourteen.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python
 
+def main():
+	try:
+		f = open('sherlock_small.txt')
+	except IOerror:
+		print "Couldn't open sherlock_small.txt"
 
+	create_traigram_dictonary(f)
+
+def create_traigram_dictonary(f):
+
+	for line in f:
+		print line
+
+
+
+
+
+if __name__ = "__main__":
+	main()
