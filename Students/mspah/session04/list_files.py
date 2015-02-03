eb4de8726ab21af11c23b5c0d4fae9a0b6bcf9ad
MattsFace <spahmatthew@gmail.com>
2015-02-03 00:08:53 -0800

    almost done with list_files.py

--- Students/mspah/session04/list_files.py
+++ Students/mspah/session04/list_files.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
+import pathlib
 
+def main():
+ 	
+ 	pth = pathlib.Path('./')
 
-
- def main():
-
- 	print os.path.abspath()
-	print os.path.relpath()
-
-
+ 	for f in pth.iterdir():
+             print f.resolve()
 
 
 if __name__ == "__main__":
