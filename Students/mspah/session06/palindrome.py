8a5211549c4996ec79aa6ec7a49b3747b1f49f20
mspah <mspah@zulily.com>
2015-02-13 15:57:02 -0800

    solved palindrome.py

--- Students/mspah/session06/palindrome.py
+++ Students/mspah/session06/palindrome.py
@@ -1,27 +1,42 @@
 #!/usr/bin/env python
 
 """
-A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-
+A palindromic number reads the same both ways.
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
-
+import sys
 
 def main():
-    # lets start by bruit force
+
     x = 100
     y = 100
+    large_pn = 0
 
 
-    while True:
+    for x in range(100, 999):
+        for y in range(100, 999):
+            pn = x * y
+            if check_pn(pn):
+                if pn > large_pn:
+                    large_pn = pn
 
+    print large_pn
 
-def multiply_by_x(x, y):
-
-
-
-def multiple_by_y(x, y):
-
+def check_pn(pn):
+    pn_string = str(pn)
+    if not len(pn_string) % 2:
+        split = len(pn_string)
+        split /= 2
+        front = pn_string[split:]
+        rear = pn_string[:split]
+        rear = rear[::-1]
+        if front == rear:
+            return True
+        else:
+            return False
+    else:
+        return False
 if __name__ == "__main__":
     main()
+
 
