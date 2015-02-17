ff31043a20c7d5d58f5a7f6c6c02f81a29b72a47
MattsFace <spahmatthew@gmail.com>
2015-02-16 19:49:56 -0800

    started on function_files

---
+++ Students/mspah/session06/functional_files.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+def main():
+
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        print "Please supply a file name"
+        sys.exit(1)
+
+    if not os.path.isfile(filename):
+        print "Unable to locate file %s" % filename
+        sys.exit(1)
+
+    overwrite = user_input()
+
+    scrub_file(filename, overwrite)
+
+
+def user_input():
+    answer = raw_input("Do you want to overwrite the file Y/N ")
+    if answer is "Y":
+        return True
+    elif answer is "N":
+        return False
+    else:
+        print "Please answer with Y or N"
+        user_input()
+
+def scrub_file(filename, overwrite):
+    new_file_name = filename + ".scrub"
+    new_file = open(new_file_name, "w")
+
+    with open(filename) as fh:
+        for line in fh:
+            line = line.strip()
+            new_file.write(line)
+
+
+if __name__ == "__main__":
+    main()
