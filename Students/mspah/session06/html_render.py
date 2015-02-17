56f0377c8a937be0a8975450775b08c394c50d8c
MattsFace <spahmatthew@gmail.com>
2015-02-16 18:42:16 -0800

    Step 1 finished

--- Students/mspah/session06/html_render.py
+++ Students/mspah/session06/html_render.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python
 
+class Element(object):
 
+
+    tag = "html"
+    indent = "  "
+
+
+    def __init__(self, content=None):
+        self.content = []
+
+        if content is not None:
+            self.content.append(content)
+
+    def append(self, new_content):
+        self.content.append(new_content)
+
+    def render(self, file, ind=""):
+
+        file.write("<" + self.tag + ">")
+        file.write('\n')
+        for line in self.content:
+            file.write(self.indent + line)
+            file.write('\n')
+        file.write("</" + self.tag + ">")
--- Students/mspah/session06/lamba_lab.py
+++ Students/mspah/session06/lamba_lab.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-
-
 def main():
 
     the_list = function_builder(5)
